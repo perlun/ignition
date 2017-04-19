b70ec16cef3a3c450d29ce74a1edb9b6cf6c720d
Per Lundberg <perlun@gmail.com>
2017-04-19 21:40:05 +0300

    Use TypeScript rather than plain JS
    For a richer editing experience.

--- api/hello.ts
+++ api/hello.ts
@@ -1,11 +1,15 @@
-function get(request, response) {
+interface IRequest {
+    pathInfo(): String;
+}
+
+function get(request: IRequest, response) {
     this.counter++;
 
     return {
         path: request.pathInfo(),
         count: this.counter || 0,
         this: this.toString(),
-        foo: _helper()
+        spam: 43
     }
 }
 
