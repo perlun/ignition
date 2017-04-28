7450e748a0b5bd4b7770a534e20754c748b8f8c5
Per Lundberg <perlun@gmail.com>
2017-04-28 08:04:14 +0200

    Bug fix: s1 == s2 means referential equality, which is not what we're after.

--- src/main/java/SparkTest/App.java
+++ src/main/java/SparkTest/App.java
@@ -18,7 +18,7 @@ public class App {
     public static void main(String[] args)
         throws Exception
     {
-        if (args.length > 0 && args[0] == "groovy") {
+        if (args.length > 0 && args[0].equals("groovy")) {
             System.out.println("Running Groovy-based engine");
             new App().runGroovyExample();
         }
